c4b6ad3687926e9f557d089b1a7e7d99c4e61f46
MTTahmid <mirza.taqi003@gmail.com>
2026-02-28 17:28:54 +0600

    Update hello.js

--- backend/hello.js
+++ backend/hello.js
@@ -1,3 +1,3 @@
 console.log("hi guys! this is tahi :)")
-console.log("Hello tahi! this is Ayiman :))")
-console.log("I am a gay little boy-taqi")
+console.log("Hello tahi! this is Ayiman I like eating poop :))")
+console.log("Hello tahi and ayiman the poop eater, I am taqi")
